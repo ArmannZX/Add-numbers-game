78f4e057f75db4a31dc5fbc93d8811b10496ac8d
Arman <arman.h16@yahoo.com>
2024-02-17 13:10:08 +0330

    Add numbers game

--- JS/Script.js
+++ JS/Script.js
@@ -2,6 +2,7 @@ let circle1 = document.getElementById("cir1");
 let circle2 = document.getElementById("cir2");
 let button = document.getElementById("BTN");
 let start = document.getElementById("Start");
+let stop = document.getElementById("Stop");
 let input = document.getElementById("input");
 let Corrects = document.getElementById("C");
 let InCorrects = document.getElementById("IC");
@@ -9,21 +10,15 @@ let Timer = document.getElementById("timer");
 let resault;
 let Cor = 0;
 let InCor = 0;
-let Time = 15;
+let Time = 100;
 let isStarted = false;
-Timer.innerText = Time;
-let width = 100;
+Timer.style.width = Time + "%";
+
 
 function MainProgram() {
     start.addEventListener("click", function () {
         if (isStarted === false) {
             isStarted = true;
-            if (Time === 0) {
-                window.alert("Too late , Your Time is up!")
-                Time = 10;
-                width = 100;
-                resault = CalculateResault();
-            }
 
             function CreateRandomNumb() {
                 let Numb1 = Math.round(Math.random() * 101)
@@ -45,55 +40,59 @@ function MainProgram() {
             resault = CalculateResault();
 
             button.addEventListener("click", function () {
-
-                if (input.value !== "") {
-                    let Answer = Number(input.value);
-                    if (Answer === resault) {
-                        let WinSound = new Audio("../Audio/Win.wav");
-                        WinSound.play();
-                        resault = CalculateResault();
-                        // window.alert("You answered correctly! You got 1 point")
-                        Time += 5;
-                        width += 15;
-                        Timer.style.width = width + "%";
-                        input.value = "";
-                        Cor++;
-                        Corrects.innerText = Cor;
-                    } else if (Answer !== resault) {
-                        let LoseSound = new Audio("../Audio/Lose.wav");
-                        LoseSound.play();
-                        resault = CalculateResault();
-                        // window.alert("Your Answer was Incorrect!")
-                        Time -= 3;
-                        width -= 5;
-                        Timer.style.width = width + "%";
-                        input.value = "";
-                        InCor++;
-                        InCorrects.innerText = InCor;
+                if (isStarted === true) {
+                    if (input.value !== "") {
+                        let Answer = Number(input.value);
+                        if (Answer === resault) {
+                            let WinSound = new Audio("./Audio/Win.wav");
+                            WinSound.play();
+                            resault = CalculateResault();
+                            // window.alert("You answered correctly! You got 1 point")
+                            Time += 20;
+                            Timer.style.width = Time + "%";
+                            input.value = "";
+                            Cor++;
+                            Corrects.innerText = Cor;
+                        } else if (Answer !== resault) {
+                            let LoseSound = new Audio("./Audio/Lose.wav");
+                            LoseSound.play();
+                            resault = CalculateResault();
+                            // window.alert("Your Answer was Incorrect!")
+                            Time -= 10;
+                            Timer.style.width = Time + "%";
+                            input.value = "";
+                            InCor++;
+                            InCorrects.innerText = InCor;
+                        }
                     }
-                } else {
-                    window.alert("Please Enter a Number!")
                 }
             })
+
             let inter = setInterval(function () {
-                let TimerSound = new Audio("../Audio/Timer.mp3");
-                Time = Time - 1;
-                Timer.innerText = Time;
+                let TimerSound = new Audio("./Audio/Timer.mp3");
+                Time = Time - 5;
+                Timer.style.width = Time + "%";
                 TimerSound.play();
-                width = width - 4;
-                Timer.style.width = width + "%";
-                if (Time === 0) {
-                    window.alert("Too late , Your Time is up!")
-                    Time = 10;
-                    width = 100;
+                if (Time < 0) {
+                    clearInterval(inter);
+                    isStarted = false;
+                    Time = 100;
                     resault = CalculateResault();
                     Cor = 0;
                     Corrects.innerText = Cor;
                     InCor = 0;
                     InCorrects.innerText = InCor;
+                    window.alert("Too late , Your Time is up!")
                 }
 
             }, 1000)
+
+            stop.addEventListener("click", function () {
+                clearInterval(inter);
+                if (isStarted === true) {
+                    isStarted = false;
+                }
+            })
         }
 
     })
